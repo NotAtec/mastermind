907984e0f8bb88b06461e0e2e457cfae5130f378
Jeroen B <jeroenb04@live.com>
2022-01-07 22:29:20 +0100

    Updated logic & input error checking
    Need to improve the logic to choose close numbers.
    It currently breaks when using multiple of the same digit

--- mastermind.rb
+++ mastermind.rb
@@ -1,18 +1,26 @@
+require 'pry-byebug'
 # frozen_string_literal: true
 
 # Class including all game info
 class Game
-
   def initialize
     @number = generate_number
   end
 
-  def choose_input
+  def choose_input()
     puts 'Guess the code the computer chose! (It has 4 digits)'
     guess = gets.chomp
     arr = guess.split('')
+    if valid_guess?(arr)
+      results = compare_digits(arr)
+      return 'winner' if results[0] == 4
 
-    compare_digits(arr)
+      puts "You got #{results[0]} digits exactly right!\n"
+      puts "And #{results[1]} of the digits were in the wrong place\n"
+      'loser'
+    else
+      'error'
+    end
   end
 
   private
@@ -23,8 +31,39 @@ class Game
     numbers
   end
 
+  def valid_guess?(arr)
+    arr.length == 4
+  end
+
   def compare_digits(arr)
+    correct = 0
+    close = 0
+
+    arr.each_with_index do |v, idx|
+      correct += 1 if v.to_i == @number[idx]
+      close += 1 if @number.include?(v.to_i) && v.to_i != @number[idx]
+    end
+    [correct, close]
   end
 end
 
+guesses = 0
 game = Game.new
+
+until guesses > 12
+  result = game.choose_input
+  case result
+  when 'winner'
+    puts 'You have chosen the correct code! Congrats'
+    break
+  when 'loser'
+    if guesses == 12 
+      puts 'You lost! Better luck next time!'
+    else
+      puts 'Try Again!'
+      guesses += 1
+    end
+  when 'error'
+    puts 'Please input 4 digits!'
+  end
+end
