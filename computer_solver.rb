04e2ca0a5df36fb2a8e2d6b8f82a730985ba540c
Jeroen B <jeroenb04@live.com>
2022-01-09 14:13:52 +0100

    Added logic to solve positions
    Can (probably) do all iterations in 4 moves or less (Needs to be tested)

--- computer_solver.rb
+++ computer_solver.rb
@@ -39,12 +39,38 @@ class ComputerSolver
       @result = results(guess)
       @sum = @result.reduce(:+)
 
+      
       @guesses = 15 if @result[0] == 4
       break if @result[0] == 4
 
       @guesses += 1
     end
 
+    unless @guesses >= 12
+      permutations = @fg.permutation.to_a
+      mutation = permutations[0]
+      result = results(mutation)
+
+      4.times do |i|
+        check = mutation.clone
+        check[i] = '10'
+        check_result = results(check)
+
+        if check_result[0] < result[0]
+          # The checked position, was correct, drop all without [x] == [i]
+          permutations.keep_if { |x| x[i] == mutation[i] }
+        else
+          # The checked position was incorrect, drop all with [x] == [i]
+          permutations.keep_if { |x| x[i] != mutation[i] }
+        end
+        @act_guess = @guesses if permutations.uniq.length == 1
+        @guesses = 15 if permutations.uniq.length == 1
+
+        break if @guesses >= 12
+
+        @guesses += 1
+      end
+    end
   end
 end
 
